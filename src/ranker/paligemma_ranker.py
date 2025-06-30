917897808eac092f8096927d57da4053bffe255c
TamChamChi <nguyenduytamanh03012004@gmail.com>
2025-06-30 21:50:32 +0700

    Refactor project structure: remove unused config files, add model and path configurations, and implement PaliGemmaRanker class in a new module. Update requirements to include more-itertools.

---
+++ src/ranker/paligemma_ranker.py
@@ -0,0 +1,239 @@
+from base import ISVGRanker
+import string
+from more_itertools import chunked
+import statistics
+import torch
+import math
+from PIL import Image
+from transformers import (
+    AutoProcessor,
+    BitsAndBytesConfig,
+    PaliGemmaForConditionalGeneration,
+)
+from src.utils.image_utils import ImageProcessor
+from src.utils.svg_utils import svg_to_png
+
+
+class PaliGemmaRanker(ISVGRanker):
+    """Evaluates images based on their similarity to a given text description using multiple choice questions."""
+
+    def __init__(self):
+        self.quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.float16,
+        )
+        self.letters = string.ascii_uppercase
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
+        self.model_path = "google/paligemma2-10b-mix-448"
+        self.processor = AutoProcessor.from_pretrained(
+            self.model_path, use_fast=True)
+        self.model = PaliGemmaForConditionalGeneration.from_pretrained(
+            self.model_path,
+            low_cpu_mem_usage=True,
+            quantization_config=self.quantization_config,
+        ).to(self.device)
+        self.question = ['Does image portray "SVG illustration of {}"?',
+                         "Do the objects in the image match the following description: {}?"]
+        self.choices = [["yes", "no"], ["yes", "no"]]
+        self.answers = ['yes', 'yes']
+
+    def score(self, questions, choices, answers, image, n=4):
+        scores = []
+        batches = (chunked(qs, n) for qs in [questions, choices, answers])
+        for question_batch, choice_batch, answer_batch in zip(*batches, strict=True):
+            scores.extend(
+                self.score_batch(
+                    image,
+                    question_batch,
+                    choice_batch,
+                    answer_batch,
+                )
+            )
+        return statistics.mean(scores)
+
+    def score_batch(
+        self,
+        image: Image.Image,
+        questions: list[str],
+        choices_list: list[list[str]],
+        answers: list[str],
+    ) -> list[float]:
+        """Evaluates the image based on multiple choice questions and answers.
+
+        Parameters
+        ----------
+        image : PIL.Image.Image
+            The image to evaluate.
+        questions : list[str]
+            List of questions about the image.
+        choices_list : list[list[str]]
+            List of lists of possible answer choices, corresponding to each question.
+        answers : list[str]
+            List of correct answers from the choices, corresponding to each question.
+
+        Returns
+        -------
+        list[float]
+            List of scores (values between 0 and 1) representing the probability of the correct answer for each question.
+        """
+        prompts = [
+            self.format_prompt(question, choices)
+            for question, choices in zip(questions, choices_list, strict=True)
+        ]
+        batched_choice_probabilities = self.get_choice_probability(
+            image, prompts, choices_list
+        )
+
+        scores = []
+        for i, _ in enumerate(questions):
+            choice_probabilities = batched_choice_probabilities[i]
+            answer = answers[i]
+            answer_probability = 0.0
+            for choice, prob in choice_probabilities.items():
+                if choice == answer:
+                    answer_probability = prob
+                    break
+            scores.append(answer_probability)
+
+        return scores
+
+    def format_prompt(self, question: str, choices: list[str]) -> str:
+        prompt = f"<image>answer en Question: {question}\nChoices:\n"
+        for i, choice in enumerate(choices):
+            prompt += f"{self.letters[i]}. {choice}\n"
+        return prompt
+
+    def mask_choices(self, logits, choices_list):
+        """Masks logits for the first token of each choice letter for each question in the batch."""
+        batch_size = logits.shape[0]
+        masked_logits = torch.full_like(logits, float("-inf"))
+
+        for batch_idx in range(batch_size):
+            choices = choices_list[batch_idx]
+            for i in range(len(choices)):
+                letter_token = self.letters[i]
+
+                first_token = self.processor.tokenizer.encode(
+                    letter_token, add_special_tokens=False
+                )[0]
+                first_token_with_space = self.processor.tokenizer.encode(
+                    " " + letter_token, add_special_tokens=False
+                )[0]
+
+                if isinstance(first_token, int):
+                    masked_logits[batch_idx, first_token] = logits[
+                        batch_idx, first_token
+                    ]
+                if isinstance(first_token_with_space, int):
+                    masked_logits[batch_idx, first_token_with_space] = logits[
+                        batch_idx, first_token_with_space
+                    ]
+
+        return masked_logits
+
+    def get_choice_probability(self, image, prompts, choices_list) -> list[dict]:
+        inputs = self.processor(
+            images=[image] * len(prompts),
+            text=prompts,
+            return_tensors="pt",
+            padding="longest",
+        ).to(self.device)
+
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            # Logits for the last (predicted) token
+            logits = outputs.logits[:, -1, :]
+            masked_logits = self.mask_choices(logits, choices_list)
+            probabilities = torch.softmax(masked_logits, dim=-1)
+
+        batched_choice_probabilities = []
+        for batch_idx in range(len(prompts)):
+            choice_probabilities = {}
+            choices = choices_list[batch_idx]
+            for i, choice in enumerate(choices):
+                letter_token = self.letters[i]
+                first_token = self.processor.tokenizer.encode(
+                    letter_token, add_special_tokens=False
+                )[0]
+                first_token_with_space = self.processor.tokenizer.encode(
+                    " " + letter_token, add_special_tokens=False
+                )[0]
+
+                prob = 0.0
+                if isinstance(first_token, int):
+                    prob += probabilities[batch_idx, first_token].item()
+                if isinstance(first_token_with_space, int):
+                    prob += probabilities[batch_idx,
+                                          first_token_with_space].item()
+                choice_probabilities[choice] = prob
+
+            # Renormalize probabilities for each question
+            total_prob = sum(choice_probabilities.values())
+            if total_prob > 0:
+                renormalized_probabilities = {
+                    choice: prob / total_prob
+                    for choice, prob in choice_probabilities.items()
+                }
+            else:
+                renormalized_probabilities = (
+                    choice_probabilities  # Avoid division by zero if total_prob is 0
+                )
+            batched_choice_probabilities.append(renormalized_probabilities)
+
+        return batched_choice_probabilities
+
+    def ocr(self, image, free_chars=4, use_num_char=False):
+        inputs = (
+            self.processor(
+                text="<image>ocr\n",
+                images=image,
+                return_tensors="pt",
+            )
+            .to(torch.float16)
+            .to(self.model.device)
+        )
+        input_len = inputs["input_ids"].shape[-1]
+
+        with torch.inference_mode():
+            outputs = self.model.generate(
+                **inputs, max_new_tokens=32, do_sample=False)
+            outputs = outputs[0][input_len:]
+            decoded = self.processor.decode(outputs, skip_special_tokens=True)
+
+        num_char = len(decoded)
+
+        # Exponentially decreasing towards 0.0 if more than free_chars detected
+        # ---------------Modified Output----------------------
+        return (
+            min(1.0, math.exp(-num_char + free_chars))
+            if not use_num_char
+            else (min(1.0, math.exp(-num_char + free_chars)), decoded)
+        )
+
+    def process(self, svg_list: list[str], prompt: str = None):
+        results = []
+
+        question = [template.format(prompt) for template in self.question]
+        choices = self.choices
+        answers = self.answers
+
+        for svg in svg_list:
+            image_processor = ImageProcessor(
+                image=svg_to_png(svg), seed=42).apply()
+            image = image_processor.image.copy()
+            score = self.score(
+                questions=question,
+                choices=choices,
+                answers=answers,
+                image=image
+            )
+            results.append({
+                "svg": svg,
+                "score": score
+            })
+        return results
+
+
