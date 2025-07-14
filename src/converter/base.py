3deee65981fc726f11ed672f276d35617d129fbd
TamChamChi <nguyenduytamanh03012004@gmail.com>
2025-07-14 17:42:35 +0700

    Add call method to IImageToConverter for streamlined image processing

--- src/converter/base.py
+++ src/converter/base.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 from PIL import Image
+
+
 class IImageToConverter(ABC):
     @abstractmethod
     def process(self, images: list[Image.Image]):
         pass
+
+    def __call__(self, images: list[Image.Image], **kwargs):
+        self.process(images, **kwargs)
